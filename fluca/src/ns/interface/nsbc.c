54ad068f9338594160c485c6fd458cdc2a77acca
Jeongu Kim <jeongu.kim@casterian.net>
2025-12-19 00:49:54 +0900

    Add missing NSBoundaryConditionTypes

--- fluca/src/ns/interface/nsbc.c
+++ fluca/src/ns/interface/nsbc.c
@@ -1,4 +1,4 @@
 #include <flucansbc.h>
 #include <petsc/private/petscimpl.h>
 
-const char *NSBoundaryConditionTypes[] = {"NONE", "VELOCITY", "PERIODIC", "NSBoundaryConditionType", "", NULL};
+const char *NSBoundaryConditionTypes[] = {"NONE", "VELOCITY", "PRESSURE_OUTLET", "PERIODIC", "SYMMETRY", "NSBoundaryConditionType", "", NULL};
