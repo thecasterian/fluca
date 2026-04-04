790dd9e5155c6b6e3653a05a09872cfbb9df836a
Jeongu Kim <jeongu.kim@casterian.net>
2026-04-05 02:56:42 +0900

    Replace Seg TGV lifecycle test with infrastructure unit tests

--- fluca/tests/seg/ex1.c
+++ fluca/tests/seg/ex1.c
@@ -1,108 +1,64 @@
-#include <flucaphys.h>
 #include <flucaseg.h>
 #include <flucasys.h>
-#include <petscdmstag.h>
-#include <petscmath.h>
 
-static const char help[] = "Verify Seg SRK lifecycle: create, setup, step, destroy\n"
-                           "Sets up a 2D TGV problem and runs one SRK step.\n";
-
-/* TGV exact solution at t=0 */
-static PetscErrorCode FillIC(DM dm, Vec Y)
-{
-  const PetscScalar **arrc[3] = {NULL, NULL, NULL};
-  PetscInt            xs, ys, xm, ym, slot_elem, i, j, c;
-  PetscReal           x, y;
-  PetscScalar         vals[3];
-  DMStagStencil       stencils[3];
-
-  PetscFunctionBegin;
-  PetscCall(DMStagGetCorners(dm, &xs, &ys, NULL, &xm, &ym, NULL, NULL, NULL, NULL));
-  PetscCall(DMStagGetProductCoordinateLocationSlot(dm, DMSTAG_ELEMENT, &slot_elem));
-  PetscCall(DMStagGetProductCoordinateArraysRead(dm, &arrc[0], &arrc[1], &arrc[2]));
-
-  for (j = ys; j < ys + ym; j++) {
-    for (i = xs; i < xs + xm; i++) {
-      x = PetscRealPart(arrc[0][i][slot_elem]);
-      y = PetscRealPart(arrc[1][j][slot_elem]);
-
-      vals[0] = -PetscCosReal(x) * PetscSinReal(y);
-      vals[1] = PetscSinReal(x) * PetscCosReal(y);
-      vals[2] = -0.25 * (PetscCosReal(2. * x) + PetscCosReal(2. * y));
-
-      for (c = 0; c < 3; c++) {
-        stencils[c].i   = i;
-        stencils[c].j   = j;
-        stencils[c].k   = 0;
-        stencils[c].loc = DMSTAG_ELEMENT;
-        stencils[c].c   = c;
-      }
-      PetscCall(DMStagVecSetValuesStencil(dm, Y, 3, stencils, vals, INSERT_VALUES));
-    }
-  }
-
-  PetscCall(DMStagRestoreProductCoordinateArraysRead(dm, &arrc[0], &arrc[1], &arrc[2]));
-  PetscCall(VecAssemblyBegin(Y));
-  PetscCall(VecAssemblyEnd(Y));
-  PetscFunctionReturn(PETSC_SUCCESS);
-}
+static const char help[] = "Test Seg lifecycle and property set/get\n";
 
 int main(int argc, char **argv)
 {
-  DM        dm, sol_dm;
-  Phys      phys;
-  Seg       seg;
-  Vec       Y;
-  PetscReal norm_before, norm_after;
+  Seg         seg;
+  SegType     type;
+  PetscReal   dt, t, max_time;
+  PetscInt    max_steps, step_number;
+  const char *prefix;
 
   PetscCall(FlucaInitialize(&argc, &argv, NULL, help));
 
-  /* Create small 2D periodic grid */
-  PetscCall(DMStagCreate2d(PETSC_COMM_WORLD, DM_BOUNDARY_PERIODIC, DM_BOUNDARY_PERIODIC, 8, 8, PETSC_DECIDE, PETSC_DECIDE, 0, 0, 1, DMSTAG_STENCIL_STAR, 1, NULL, NULL, &dm));
-  PetscCall(DMSetFromOptions(dm));
-  PetscCall(DMSetUp(dm));
-  PetscCall(DMStagSetUniformCoordinatesProduct(dm, 0., 2. * PETSC_PI, 0., 2. * PETSC_PI, 0., 0.));
+  /* Create and destroy empty Seg (no type set) */
+  PetscCall(SegCreate(PETSC_COMM_WORLD, &seg));
+  PetscCall(SegDestroy(&seg));
 
-  /* Create PhysINS */
-  PetscCall(PhysCreate(PETSC_COMM_WORLD, &phys));
-  PetscCall(PhysSetType(phys, PHYSINS));
-  PetscCall(PhysSetBaseDM(phys, dm));
-  PetscCall(PhysINSSetDensity(phys, 1.));
-  PetscCall(PhysINSSetViscosity(phys, 0.01));
-  PetscCall(PhysSetFromOptions(phys));
-  PetscCall(PhysSetUp(phys));
-  PetscCall(PhysGetSolutionDM(phys, &sol_dm));
-
-  /* Create Seg SRK and wire operators */
+  /* Create, set type, query type */
   PetscCall(SegCreate(PETSC_COMM_WORLD, &seg));
   PetscCall(SegSetType(seg, SEGSRK));
-  PetscCall(PhysSetUpSeg(phys, seg));
+  PetscCall(SegGetType(seg, &type));
+  PetscCall(PetscPrintf(PETSC_COMM_WORLD, "Type: %s\n", type));
 
-  /* Set solution and time parameters */
-  PetscCall(DMCreateGlobalVector(sol_dm, &Y));
-  PetscCall(FillIC(sol_dm, Y));
-  PetscCall(SegSetSolution(seg, Y));
-  PetscCall(SegSetTimeStepSize(seg, 0.01));
-  PetscCall(SegSetMaxSteps(seg, 1));
-  PetscCall(SegSetFromOptions(seg));
-  PetscCall(SegSetUp(seg));
+  /* Default time properties */
+  PetscCall(SegGetTime(seg, &t));
+  PetscCall(SegGetStepNumber(seg, &step_number));
+  PetscCall(PetscPrintf(PETSC_COMM_WORLD, "Default time: %g\n", (double)t));
+  PetscCall(PetscPrintf(PETSC_COMM_WORLD, "Default step number: %" PetscInt_FMT "\n", step_number));
 
-  /* Record norm before stepping */
-  PetscCall(VecNorm(Y, NORM_2, &norm_before));
-  PetscCheck(PetscIsNormalReal(norm_before), PETSC_COMM_WORLD, PETSC_ERR_FP, "Initial solution norm is not finite");
+  /* Set and get time step size */
+  PetscCall(SegSetTimeStepSize(seg, 0.005));
+  PetscCall(SegGetTimeStepSize(seg, &dt));
+  PetscCall(PetscPrintf(PETSC_COMM_WORLD, "dt: %g\n", (double)dt));
 
-  /* Run one step */
-  PetscCall(SegStep(seg));
+  /* Set and get time */
+  PetscCall(SegSetTime(seg, 1.5));
+  PetscCall(SegGetTime(seg, &t));
+  PetscCall(PetscPrintf(PETSC_COMM_WORLD, "t: %g\n", (double)t));
 
-  /* Verify solution is still finite after stepping */
-  PetscCall(VecNorm(Y, NORM_2, &norm_after));
-  PetscCheck(PetscIsNormalReal(norm_after), PETSC_COMM_WORLD, PETSC_ERR_FP, "Solution norm is not finite after step");
+  /* Set and get max time */
+  PetscCall(SegSetMaxTime(seg, 10.0));
+  PetscCall(SegGetMaxTime(seg, &max_time));
+  PetscCall(PetscPrintf(PETSC_COMM_WORLD, "max_time: %g\n", (double)max_time));
 
-  /* Cleanup */
-  PetscCall(VecDestroy(&Y));
+  /* Set and get max steps */
+  PetscCall(SegSetMaxSteps(seg, 200));
+  PetscCall(SegGetMaxSteps(seg, &max_steps));
+  PetscCall(PetscPrintf(PETSC_COMM_WORLD, "max_steps: %" PetscInt_FMT "\n", max_steps));
+
+  /* Options prefix */
+  PetscCall(SegSetOptionsPrefix(seg, "test_"));
+  PetscCall(SegGetOptionsPrefix(seg, &prefix));
+  PetscCall(PetscPrintf(PETSC_COMM_WORLD, "Prefix: %s\n", prefix));
+
+  PetscCall(SegAppendOptionsPrefix(seg, "sub_"));
+  PetscCall(SegGetOptionsPrefix(seg, &prefix));
+  PetscCall(PetscPrintf(PETSC_COMM_WORLD, "Prefix after append: %s\n", prefix));
+
   PetscCall(SegDestroy(&seg));
-  PetscCall(PhysDestroy(&phys));
-  PetscCall(DMDestroy(&dm));
 
   PetscCall(FlucaFinalize());
 }
@@ -110,75 +66,7 @@ int main(int argc, char **argv)
 /*TEST
 
   test:
-    suffix: ars111
+    suffix: lifecycle
     nsize: 1
-    args: -seg_srk_type ars111
-    output_file: output/empty.out
-
-  test:
-    suffix: ars121
-    nsize: 1
-    args: -seg_srk_type ars121
-    output_file: output/empty.out
-
-  test:
-    suffix: ars222
-    nsize: 1
-    args: -seg_srk_type ars222
-    output_file: output/empty.out
-
-  test:
-    suffix: ars232
-    nsize: 1
-    args: -seg_srk_type ars232
-    output_file: output/empty.out
-
-  test:
-    suffix: ars343
-    nsize: 1
-    args: -seg_srk_type ars343
-    output_file: output/empty.out
-
-  test:
-    suffix: ars443
-    nsize: 1
-    args: -seg_srk_type ars443
-    output_file: output/empty.out
-
-  test:
-    suffix: ark324l2sa
-    nsize: 1
-    args: -seg_srk_type ark324l2sa
-    output_file: output/empty.out
-
-  test:
-    suffix: ark436l2sa
-    nsize: 1
-    args: -seg_srk_type ark436l2sa
-    output_file: output/empty.out
-
-  test:
-    suffix: ark548l2sa
-    nsize: 1
-    args: -seg_srk_type ark548l2sa
-    output_file: output/empty.out
-
-  test:
-    suffix: mark324l2sa
-    nsize: 1
-    args: -seg_srk_type mark324l2sa
-    output_file: output/empty.out
-
-  test:
-    suffix: mars343
-    nsize: 1
-    args: -seg_srk_type mars343
-    output_file: output/empty.out
-
-  test:
-    suffix: bhr553
-    nsize: 1
-    args: -seg_srk_type bhr553
-    output_file: output/empty.out
 
 TEST*/
