cdcbb847826e3165438e9429ed0a6c58e5d5eb12
Jeongu Kim <jeongu.kim@casterian.net>
2026-04-05 03:10:49 +0900

    Remove old NS module, its tests, and the app

--- fluca/src/ns/interface/nsbc.c
+++
@@ -1,4 +0,0 @@
-#include <flucansbc.h>
-#include <petsc/private/petscimpl.h>
-
-const char *NSBoundaryConditionTypes[] = {"NONE", "VELOCITY", "PRESSURE_OUTLET", "PERIODIC", "SYMMETRY", "NSBoundaryConditionType", "", NULL};
